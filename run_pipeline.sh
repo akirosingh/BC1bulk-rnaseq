f75727fc7de6ef2db71d2d45696d4c69e7dda83d
akirosingh <adamsk@stanford.edu>
2021-05-27 11:25:18 -0700

    Updating samples 🤩️

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=bulk-rnaseq
-#SBATCH --output=/scratch/users/tbencomo/logs/bulk-rnaseq.out
+#SBATCH --job-name=BC1bulk-rnaseq
+#SBATCH --output=/scratch/users/adamsk/logs/bulk-rnaseq.out
 #SBATCH --nodes=1
-#SBATCH --time=00-04:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=8000
+#SBATCH --time=00-05:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100G
 #SBATCH --mail-type=END
-#SBATCH --mail-user=tbencomo@stanford.edu
+#SBATCH --mail-user=adamsk@stanford.edu
+#SBATCH -p dschneid
 
 set -e
 cd $1
